e5d1473113bea14427454431cf99de666d15dad2
JulienArmand <julien.armand26@gmail.com>
2016-02-10 17:31:55 +0100

    Cette fois c'est la bonne

---
+++ ChatServer.java
@@ -0,0 +1,12 @@
+import java.rmi.server.*;
+import java.rmi.registry.*;
+public class ChatServer {
+	public static void main(String [] args){
+		try{
+			
+		} catch(Exception e){
+			System.err.println("Error on server : "+e);
+			e.printStackTrace();
+		}
+	}
+}
